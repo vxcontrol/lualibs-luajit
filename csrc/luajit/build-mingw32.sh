9a1f8a577ebb272c252e2cb5bb9d5a960fb65556
Mikhail Kochegarov <mikhail@kochegarov.biz>
2022-12-19 16:03:23 +1000

    fix build scripts for mingw (replace artefact name with luajit-bin)

--- csrc/luajit/build-mingw32.sh
+++ csrc/luajit/build-mingw32.sh
@@ -2,4 +2,4 @@ export LUA_PATH_DEFAULT=".\\\\?.lua;!\\\\..\\\\..\\\\?.lua;!\\\\..\\\\..\\\\?\\\
 export LUA_CPATH_DEFAULT=".\\\\?.dll;!\\\\clib\\\\?.dll;!\\\\loadall.dll"
 [ `uname` = Linux ] && { export CROSS=i686-w64-mingw32-; export TARGET_SYS=Windows; }
 [[ `uname` = *MINGW* ]] && { export MAKE=mingw32-make; }
-P=mingw32 HOST_CC="gcc -m32" CFLAGS="-fPIC" LDFLAGS=-static-libgcc X=luajit.exe D=lua51.dll A=luajit.a ./build.sh
+P=mingw32 HOST_CC="gcc -m32" CFLAGS="-fPIC" LDFLAGS=-static-libgcc X0=luajit.exe X=luajit-bin.exe D=lua51.dll A=luajit.a ./build.sh
--- csrc/luajit/build-mingw64.sh
+++ csrc/luajit/build-mingw64.sh
@@ -2,4 +2,4 @@ export LUA_PATH_DEFAULT=".\\\\?.lua;!\\\\..\\\\..\\\\?.lua;!\\\\..\\\\..\\\\?\\\
 export LUA_CPATH_DEFAULT=".\\\\?.dll;!\\\\clib\\\\?.dll;!\\\\loadall.dll"
 [ `uname` = Linux ] && { export CROSS=x86_64-w64-mingw32-; export TARGET_SYS=Windows; }
 [[ `uname` = *MINGW* ]] && { export MAKE=mingw32-make; }
-P=mingw64 HOST_CC="gcc" CFLAGS="-fPIC" LDFLAGS=-static-libgcc X=luajit.exe D=lua51.dll A=luajit.a ./build.sh
+P=mingw64 HOST_CC="gcc" CFLAGS="-fPIC" LDFLAGS=-static-libgcc X0=luajit.exe X=luajit-bin.exe D=lua51.dll A=luajit.a ./build.sh
